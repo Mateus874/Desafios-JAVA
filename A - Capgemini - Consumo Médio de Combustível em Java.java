0d583f65ad7a1f8902934cd1d809fa2983096399
Mateus Oliveira <158223377+Mateus874@users.noreply.github.com>
2024-02-26 11:02:18 -0300

    Update and rename A - Capgemini - Consumo Médio de Combustível em Java to A - Capgemini - Consumo Médio de Combustível em Java.java

--- A - Capgemini - Consumo Médio de Combustível em Java.java
+++ A - Capgemini - Consumo Médio de Combustível em Java.java
@@ -1,6 +1,6 @@
-- 3 - Escreva um algoritmo para determinar o consumo médio de um automóvel sendo fornecida a distância total percorrida pelo automóvel e o total de combustível gasto;
+/* 3 - Escreva um algoritmo para determinar o consumo médio de um automóvel sendo fornecida a distância total percorrida pelo automóvel e o total de combustível gasto;
     
-    ```java
+    ```java*/
     public static void main(String[] args) {  
             float distânciaTotal;
             float combustívelGasto;
