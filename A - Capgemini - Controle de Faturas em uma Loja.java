d2b5dfc28ba09474750304282089145de665cd56
Mateus Oliveira <158223377+Mateus874@users.noreply.github.com>
2024-02-26 10:59:38 -0300

    Update and rename A - Capgemini - Controle de Faturas em uma Loja to A - Capgemini - Controle de Faturas em uma Loja.java

--- A - Capgemini - Controle de Faturas em uma Loja.java
+++ A - Capgemini - Controle de Faturas em uma Loja.java
@@ -1,4 +1,4 @@
------------------------------------------------------------
+/*-----------------------------------------------------------
 6. Crie uma classe chamada Invoice que possa ser utilizado por uma loja de suprimentos de informática para
  representar uma fatura de um item vendido na loja. Uma fatura deve incluir as seguintes informações
 como atributos:
@@ -12,7 +12,7 @@ positiva, ela deve ser configurada como 0. Se o preço por item não for positiv
 Forneça os métodos getters e setters para cada variável de instância. Além disso, forneça um método
 chamado getInvoiceAmount que calcula o valor da fatura (isso é, multiplica a quantidade pelo preço por item) e depois
 retorna o valor como um double. Escreva um aplicativo de teste que demonstra as capacidades da classe Invoice.
------------------------------------------------------------------
+-----------------------------------------------------------------*/
 
 
 //class método
