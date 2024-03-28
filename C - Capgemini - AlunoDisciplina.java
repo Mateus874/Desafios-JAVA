2e69ba48ddb4c87be9de8d4d41a83c299aba01a4
Mateus Oliveira <158223377+Mateus874@users.noreply.github.com>
2024-03-28 00:32:14 -0300

    Update and rename B - Capgemini - AlunoDisciplina to C - Capgemini - AlunoDisciplina.java

--- C - Capgemini - AlunoDisciplina.java
+++ C - Capgemini - AlunoDisciplina.java
@@ -1,10 +1,10 @@
---------------------------------------------------------------------
+/*--------------------------------------------------------------------
 5. Escreva uma classe cujos objetos representam alunos matriculados em uma disciplina. Cada objeto dessa
 classe deve guardar os seguintes dados do aluno: matrícula, nome, 2 notas de prova e 1 nota de trabalho.
 Escreva os seguintes métodos para esta classe:
 a. media: calcula a média final do aluno (cada prova tem peso 2,5 e o trabalho tem peso 2)
 b. final: calcula quanto o aluno precisa para a prova final (retorna zero se ele não for para a final)
-------------------------------------------------------------------
+------------------------------------------------------------------*/
 public void calcularMédia(){
                                     
 
