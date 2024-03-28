0590b152a59d26b19b04b492fa2bb083f26b8605
Mateus Oliveira <158223377+Mateus874@users.noreply.github.com>
2024-03-28 00:34:01 -0300

    Update and rename Capgemini - Troca de Valores em Java to C - Capgemini - Troca de Valores.java

--- C - Capgemini - Troca de Valores.java
+++ C - Capgemini - Troca de Valores.java
@@ -1,8 +1,7 @@
-- 6 - Leia dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variável A passe a
+/* 6 - Leia dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variável A passe a
 possuir o valor da variável B e a variável B passe a possuir o valor da variável A. Apresentar os valores
-trocados;
+trocados;*/
     
-    ```java
     System.out.print("Valor A: ");
             float valorA = leitorScanner.nextFloat();
             
@@ -14,4 +13,4 @@ trocados;
             valorB = valorC;
             System.out.println("O valor de A é: " + valorA + "e B tem o valor de: " + valorB);
             }
-    ```
+
