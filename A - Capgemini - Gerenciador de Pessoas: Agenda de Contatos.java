687fbd544eace81a3f12f7c5e3e47ba8fdaf29d3
Mateus Oliveira <158223377+Mateus874@users.noreply.github.com>
2024-03-28 00:43:38 -0300

    Update and rename A - Capgemini - Gerenciador de Pessoas: Agenda de Contatos to A - Capgemini - Gerenciador de Pessoas: Agenda de Contatos.java

--- A - Capgemini - Gerenciador de Pessoas: Agenda de Contatos.java
+++ A - Capgemini - Gerenciador de Pessoas: Agenda de Contatos.java
@@ -1,4 +1,4 @@
----------------------------------------------
+/*---------------------------------------------
 12. Crie uma classe Agenda que pode armazenar 10 pessoas e que seja capaz de realizar as seguintes
 operações:
 a. void armazenarPessoa(String nome, int idade, float altura); //armazena a pessoa em um array
@@ -6,7 +6,7 @@ b. void removerPessoa(String nome); //remove a pessoa do array
 c. int buscarPessoa(String nome); // informa em que posição da agenda está a pessoa
 d. void imprimirAgenda(); // imprime os dados de todas as pessoas da agenda
 e. void imprimirPessoa(int index); // imprime os dados da pessoa que está na posição “i” da agenda. 
------------------------------------------------------------
+-----------------------------------------------------------*/
 package AtividadePOO;
 
 import java.util.ArrayList;
